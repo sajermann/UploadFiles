d24f5ddd7c917e3c02a98e2b907dcf18b03beb32
Bruno Sajermann <sajermannbruno@gmail.com>
2022-10-31 08:15:32 -0300

    console.log

--- front/src/Services/Upload/index.test.ts
+++ front/src/Services/Upload/index.test.ts
@@ -12,8 +12,10 @@ const baseUrl = '/upload';
 describe.only('Pages/ForMockTest', () => {
 	it(`should mock method umaFuncaoQualquer`, async () => {
 		apiMock.onAny(`${baseUrl}/batata`).replyOnce(200, { data: 'test' });
-
+		console.log('Antes req');
 		const t = await uploadService.postUpload();
+		console.log('Depois resp');
 		expect(JSON.stringify(t)).toBe(JSON.stringify({ data: 'test' }));
+		console.log('Depois expect');
 	});
 });
