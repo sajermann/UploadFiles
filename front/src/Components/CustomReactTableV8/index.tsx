a5c746904b88d518eee35f84d415df9d4db1f71a
Bruno Sajermann <sajermannbruno@gmail.com>
2022-11-06 01:25:24 -0300

    v8

--- front/src/Components/CustomReactTableV8/index.tsx
+++ front/src/Components/CustomReactTableV8/index.tsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-/* eslint-disable react/button-has-type */
-import React from 'react';
-
+import React, { HTMLProps, useEffect, useRef } from 'react';
 import './index.css';
 
 import {
@@ -10,77 +9,121 @@ import {
 	flexRender,
 	getCoreRowModel,
 	getSortedRowModel,
+	OnChangeFn,
 	Row,
+	RowSelectionState,
 	SortingState,
 	useReactTable,
 } from '@tanstack/react-table';
 import { useVirtual } from 'react-virtual';
-import { makeData, Person } from './makeData';
 
-export function CustomReactTableV8() {
+type Props<T> = {
+	selection?: {
+		type: 'multi' | 'single';
+		rowSelection: { [index: number]: boolean };
+		setRowSelection: OnChangeFn<RowSelectionState>;
+	};
+	columns: ColumnDef<T>[];
+	data: T[];
+	isLoading?: boolean;
+};
+
+function IndeterminateCheckbox({
+	indeterminate,
+	className = '',
+	...rest
+}: { indeterminate?: boolean } & HTMLProps<HTMLInputElement>) {
+	const ref = useRef<HTMLInputElement>(null!);
+
+	useEffect(() => {
+		if (typeof indeterminate === 'boolean') {
+			ref.current.indeterminate = !rest.checked && indeterminate;
+		}
+	}, [ref, indeterminate]);
+
+	return (
+		<input
+			type="checkbox"
+			ref={ref}
+			className={`${className} cursor-pointer`}
+			{...rest}
+		/>
+	);
+}
+
+export function CustomReactTableV8<T>({
+	selection,
+	columns,
+	data,
+	isLoading,
+}: Props<T>) {
 	const [sorting, setSorting] = React.useState<SortingState>([]);
 
-	const columns = React.useMemo<ColumnDef<Person>[]>(
-		() => [
-			{
-				accessorKey: 'id',
-				header: 'ID',
-				size: 60,
-				align: 'center',
-				minSize: 2000,
-			},
-			{
-				accessorKey: 'firstName',
-				cell: info => info.getValue(),
-			},
-			{
-				accessorFn: row => row.lastName,
-				id: 'lastName',
-				cell: info => info.getValue(),
-				header: () => <span>Last Name</span>,
-			},
-			{
-				accessorKey: 'age',
-				header: () => 'Age',
-				size: 50,
-			},
-			{
-				accessorKey: 'visits',
-				header: () => <span>Visits</span>,
-				size: 50,
-			},
-			{
-				accessorKey: 'status',
-				header: 'Status',
-			},
-			{
-				accessorKey: 'progress',
-				header: 'Profile Progress',
-				size: 80,
-			},
-			{
-				accessorKey: 'createdAt',
-				header: 'Created At',
-				cell: info => info.getValue<Date>().toLocaleString(),
-			},
-		],
-		[]
-	);
+	function buildColumns() {
+		if (selection) {
+			const t = [
+				{
+					id: 'select',
+					header: ({ table }: any) =>
+						selection.type === 'multi' && (
+							<IndeterminateCheckbox
+								{...{
+									checked: table.getIsAllRowsSelected(),
+									indeterminate: table.getIsSomeRowsSelected(),
+									onChange: table.getToggleAllRowsSelectedHandler(),
+								}}
+							/>
+						),
 
-	const [data, setData] = React.useState(() => makeData(50_000));
-	const refreshData = () => setData(() => makeData(50_000));
+					size: 60,
+					minSize: 60,
+					maxSize: 60,
+					align: 'center',
+					enableSorting: false,
+					cell: ({ row }: any) => (
+						<div className="px-1">
+							<IndeterminateCheckbox
+								{...{
+									checked: row.getIsSelected(),
+									indeterminate: row.getIsSomeSelected(),
+								}}
+							/>
+						</div>
+					),
+				},
+			];
+			return [...t, ...columns];
+		}
+		return columns;
+	}
 
 	const table = useReactTable({
 		data,
-		columns,
+		columns: buildColumns(),
+		columnResizeMode: 'onChange',
 		state: {
 			sorting,
+			rowSelection: selection?.rowSelection,
 		},
+		onRowSelectionChange: selection?.setRowSelection,
+		enableRowSelection: selection !== undefined,
+		enableMultiRowSelection: selection?.type === 'multi',
 		onSortingChange: setSorting,
 		getCoreRowModel: getCoreRowModel(),
 		getSortedRowModel: getSortedRowModel(),
 		debugTable: true,
 	});
+
+	function verifyIsSelected(row: Row<T>) {
+		try {
+			if (row.getIsSelected()) {
+				return 'active';
+			}
+			return '';
+		} catch {
+			return '';
+		}
+	}
 
 	const tableContainerRef = React.useRef<HTMLDivElement>(null);
 
@@ -100,8 +143,31 @@ export function CustomReactTableV8() {
 
 	return (
 		<div className="p-2">
+			{isLoading && (
+				<div
+					style={{
+						// backgroundColor: 'rgba(0,0,0,0.1)',
+						height: '55%',
+						position: 'absolute',
+						width: '97.78%',
+						top: '90px',
+						zIndex: 1,
+					}}
+				>
+					<div
+						style={{
+							height: '100%',
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'center',
+						}}
+					>
+						Carregando...
+					</div>
+				</div>
+			)}
 			<div className="h-2" />
-			<div ref={tableContainerRef} className="container">
+			<div ref={tableContainerRef} className="customContainer">
 				<table>
 					<thead>
 						{table.getHeaderGroups().map(headerGroup => (
@@ -110,44 +176,79 @@ export function CustomReactTableV8() {
 									<th
 										key={header.id}
 										colSpan={header.colSpan}
-										style={{ width: header.getSize() }}
+										style={{
+											width: header.getSize(),
+											// @ts-expect-error dasddas
+											textAlign: header.getContext().column.columnDef.align,
+										}}
 									>
 										{header.isPlaceholder ? null : (
-											<div
-												{...{
-													className: header.column.getCanSort()
-														? 'cursor-pointer select-none'
-														: '',
-													onClick: header.column.getToggleSortingHandler(),
-												}}
-											>
-												{flexRender(
-													header.column.columnDef.header,
-													header.getContext()
-												)}
-												{{
-													asc: ' 🔼',
-													desc: ' 🔽',
-												}[header.column.getIsSorted() as string] ?? null}
-											</div>
+											<>
+												<div
+													{...{
+														className: header.column.getCanSort()
+															? 'cursor-pointer select-none'
+															: '',
+														onClick: header.column.getToggleSortingHandler(),
+													}}
+												>
+													{flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+													)}
+													{{
+														asc: ' 🔼',
+														desc: ' 🔽',
+													}[header.column.getIsSorted() as string] ?? null}
+												</div>
+												{/* SizingMode */}
+												<div
+													{...{
+														onMouseDown: header.getResizeHandler(),
+														onTouchStart: header.getResizeHandler(),
+														className: `resizer ${
+															header.column.getIsResizing() ? 'isResizing' : ''
+														}`,
+													}}
+												/>
+											</>
 										)}
 									</th>
 								))}
 							</tr>
 						))}
 					</thead>
-					<tbody>
+					<tbody style={{ opacity: isLoading ? 0.5 : 1 }}>
+						{data.length === 0 && !isLoading && (
+							<tr style={{ height: '100%' }}>
+								<td
+									colSpan={Object.keys(columns).length}
+									style={{ border: '1px solid', textAlign: 'center' }}
+								>
+									Sem Dados
+								</td>
+							</tr>
+						)}
+
 						{paddingTop > 0 && (
 							<tr>
 								<td style={{ height: `${paddingTop}px` }} />
 							</tr>
 						)}
 						{virtualRows.map(virtualRow => {
-							const row = rows[virtualRow.index] as Row<Person>;
+							const row = rows[virtualRow.index] as Row<T>;
 							return (
-								<tr key={row.id}>
+								<tr
+									key={row.id}
+									className={verifyIsSelected(row)}
+									onClick={() => row.toggleSelected()}
+								>
 									{row.getVisibleCells().map(cell => (
-										<td key={cell.id}>
+										<td
+											key={cell.id}
+											// @ts-expect-error dasddas
+											style={{ textAlign: cell.column.columnDef.align }}
+										>
 											{flexRender(
 												cell.column.columnDef.cell,
 												cell.getContext()
@@ -166,9 +267,6 @@ export function CustomReactTableV8() {
 				</table>
 			</div>
 			<div>{table.getRowModel().rows.length} Rows</div>
-			<div>
-				<button onClick={() => refreshData()}>Refresh Data</button>
-			</div>
 		</div>
 	);
 }
--- front/src/Pages/CustomReactTableV8/index.tsx
+++ front/src/Pages/CustomReactTableV8/index.tsx
@@ -1,16 +1,86 @@
-import { useState } from 'react';
+/* eslint-disable react/no-unstable-nested-components */
+import { useState, useMemo, useEffect } from 'react';
+import { ColumnDef } from '@tanstack/react-table';
 import styles from './index.module.css';
-import 'react-data-grid/lib/styles.css';
-import data from '../../Data/tags.json';
 import { CustomReactTableV8 } from '../../Components/CustomReactTableV8';
+import { makeData } from '../../Components/CustomReactTableV8/makeData';
+
+type Person = {
+	id: number;
+	firstName: string;
+	lastName: string;
+	age: number;
+	visits: number;
+	progress: number;
+	status: 'relationship' | 'complicated' | 'single';
+	createdAt: Date;
+};
 
 export default function CustomReactTableV8Page() {
 	const [isLoading, setIsLoading] = useState(false);
-	const [selectedRows, setSelectedRows] = useState<object[]>([]);
+	const [rowSelection, setRowSelection] = useState({});
+	const [data, setData] = useState(() => makeData(100));
+
+	const columns = useMemo<ColumnDef<Person>[]>(
+		() => [
+			{
+				accessorKey: 'id',
+				header: 'ID',
+				size: 60,
+				align: 'center',
+				minSize: 220,
+				enableSorting: false,
+				batata: () => console.log('Batata'),
+			},
+			{
+				accessorKey: 'firstName',
+				cell: info => info.getValue(),
+				align: 'center',
+			},
+			{
+				accessorFn: row => row.lastName,
+				id: 'lastName',
+				cell: info => info.getValue(),
+				header: () => <span>Last Name</span>,
+			},
+			{
+				accessorKey: 'age',
+				header: () => 'Age',
+				size: 50,
+			},
+			{
+				accessorKey: 'visits',
+				header: () => <span>Visits</span>,
+				size: 600,
+			},
+			{
+				accessorKey: 'status',
+				header: 'Status',
+			},
+			{
+				accessorKey: 'progress',
+				header: 'Profile Progress',
+				size: 80,
+			},
+			{
+				accessorKey: 'createdAt',
+				header: 'Created At',
+				cell: info => info.getValue<Date>().toLocaleString(),
+			},
+		],
+		[]
+	);
+
+	useEffect(() => console.log(rowSelection), [rowSelection]);
 
 	return (
 		<div className={styles.customContainer}>
-			<CustomReactTableV8 />
+			<CustomReactTableV8
+				columns={columns}
+				data={data}
+				selection={{ type: 'single', rowSelection, setRowSelection }}
+				// isLoading
+			/>
 		</div>
 	);
 }
